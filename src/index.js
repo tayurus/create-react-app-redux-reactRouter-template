ccd16e688ee085e45df2fb19c75d393ed91952d0
tayurus <tayurus@gmail.com>
2018-11-21 21:18:22 +0300

    add bootstrap and rem

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import * as serviceWorker from "./serviceWorker";
 
 import { Provider } from "react-redux";
